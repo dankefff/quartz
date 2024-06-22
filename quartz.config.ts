9464054badc3da328adf520ddca101f6385bb02e
dankefff <95320614+dankefff@users.noreply.github.com>
2024-06-22 17:25:06 +0900

    Update quartz.config.ts
    페이지 타이틀 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴개인페이지",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
